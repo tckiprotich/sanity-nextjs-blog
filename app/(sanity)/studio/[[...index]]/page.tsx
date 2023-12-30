f88c313f8ddbbd81a03b6abb1802cc52d437dd55
tckiprotich <tckiprotich@outlook.com>
2023-12-30 05:19:40 +0000

    Update dependencies and comment out unused code

--- app/(sanity)/studio/[[...index]]/page.tsx
+++ app/(sanity)/studio/[[...index]]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { NextStudio } from "next-sanity/studio";
 import config from "sanity.config";
-export const runtime = 'edge';
+// export const runtime = 'edge';
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
